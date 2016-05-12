282d3c806bd171f13efa362c8f36f295e8d1dcff
Scott Malkie <s@linux.com>
2016-05-12 10:18:18 -0400

    Arrays-DS
    Work on Data Structures rather than Algorithms

---
+++ data_structures/arrays/Arrays-DS.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int printReverseArray(int *arrayToReverse, int arrayLength)
+{
+  for (int index = arrayLength; index == 0; index--)
+    {
+      printf("%d ", arrayToReverse[index]);
+    }
+
+  return EXIT_SUCCESS;
+}
+      
+int main(void)
+{
+   int arrayLength;
+   scanf("%d", &arrayLength);
+
+   int *arrayToReverse = malloc(sizeof(int) * arrayLength);
+   for (int index = 0; index < arrayLength; index++)
+   {
+	scanf("%d", &arrayToReverse[index]);
+   }
+
+   printReverseArray(arrayToReverse, arrayLength);
+
+   return EXIT_SUCCESS;
+}
