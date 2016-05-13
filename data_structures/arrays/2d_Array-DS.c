d5706fea57d88e2dc287d303e7c639c4f91d6998
Scott Malkie <s [t] linux [dt] com>
2016-05-13 08:56:22 -0400

    Finish 2D Array-DS
    Flatten the 2D Array into 1D and use Pivots to calculate hourglasses.
    Not usable on anything besides 6x6 arrays.

--- data_structures/arrays/2d_Array-DS.c
+++ data_structures/arrays/2d_Array-DS.c
@@ -5,14 +5,30 @@
 
 int calculateLargestHourglass(int *hourglassArray, int arraysize)
 {
-   int index = arraysize - 1;
-   while (index >= 0)
-   {
-      printf("%d ", hourglassArray[index]);
-      index--;
-   }
    
-   return EXIT_SUCCESS;
+   int hourglassIndexPivots[16] = {7, 8, 9, 10, 13, 14, 15, 16, 19, 22, 21, 22, 25, 26, 27, 28};
+   
+   int largestHourglassSum = 0;
+   
+   // hourglass == [-7][-6][-5][pivot][+5][+6][+7] 
+   for (int index = 0; index < 16; index++)
+   {
+      int pivotIndex = hourglassIndexPivots[index]; 
+      int hourglassSum = hourglassArray[(pivotIndex - 7)] + \
+                         hourglassArray[(pivotIndex - 6)] + \
+                         hourglassArray[(pivotIndex - 5)] + \
+                         hourglassArray[pivotIndex]       + \
+                         hourglassArray[(pivotIndex + 5)] + \
+                         hourglassArray[(pivotIndex + 6)] + \
+                         hourglassArray[(pivotIndex + 7)];
+      
+      if (hourglassSum > largestHourglassSum)
+      {
+         largestHourglassSum = hourglassSum;
+      }
+   }
+      
+   return largestHourglassSum;
 }
 
 int main(void)
@@ -24,7 +40,7 @@ int main(void)
 	   scanf("%d", &hourglassArray[index]);
       }
 
-   calculateLargestHourglass(hourglassArray, SIZE_OF_ARRAY);
+   printf("%d", calculateLargestHourglass(hourglassArray, SIZE_OF_ARRAY));
 
    return EXIT_SUCCESS;
 }
