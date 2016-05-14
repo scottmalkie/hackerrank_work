22fa994506d40cf88eef9f544888c2bf66268f65
Scott Malkie <s@linux.com>
2016-05-14 18:08:22 -0400

    More Work on Dynamic Array

--- data_structures/arrays/dynamic_array.c
+++ data_structures/arrays/dynamic_array.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int seqList[1000000];
+int lastAns = 0;
+
+int query1(int seqIndex, int appendInt)
+{
+   return seqIndex;
+}
+
+int query2(int seqIndex)
+{
+   printf("%d\n", lastAns);
+   
+   return seqIndex;
+}
+
+
 int main(void)
 {
    int numberOfTestcases;
