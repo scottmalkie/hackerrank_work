369f0faf9d9cfb02019958ea3370dd878517fb11
Scott Malkie <s@linux.com>
2016-05-17 21:18:18 -0400

    More Work on Linked Lists

--- data_structures/linked_lists/Print_the_Elements_of_a_Linked_List.c
+++ data_structures/linked_lists/Print_the_Elements_of_a_Linked_List.c
@@ -3,4 +3,34 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+struct Node
+{
+	 int data;
+	 struct Node *next;
+}
 
+void Print(Node *head)
+{
+	printf("%d", Node->data);
+}
+
+int main(void)
+{
+	for (int index = 0; ; index++)
+	{
+		if ((Node->next * index) != NULL)
+		{
+			Printf(Node->next * index);
+		}
+		else if ((Node->next * index) == NULL)
+		{
+			printf("Reached end of list");
+			return EXIT_SUCCESS;
+		}
+		else 
+		{
+			printf("Error parsing linked list!")
+			return EXIT_FAILURE;
+		}
+
+}
