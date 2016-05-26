5ea032dfa9df2cbd9758b2022a9a797d1c9ebfe8
Scott Malkie <s@linux.com>
2016-05-25 21:54:09 -0400

    More Work on Linked Lists
    Regression....

--- data_structures/linked_lists/Print_the_Elements_of_a_Linked_List.c
+++ data_structures/linked_lists/Print_the_Elements_of_a_Linked_List.c
@@ -4,53 +4,44 @@
 
 typedef struct Node
 {
-	 int data;
-	 struct Node* next;
+     int data;
+     struct Node* next;
 } Node;
 
 int printData(Node* element)
 {
-	Node* traversal;
-	traversal = element;
-	
-	while (traversal->next != NULL)
-	{
-		traversal = traversal->next;
-		printf("%d\n", traversal->data);
-	}
-	
-	return EXIT_SUCCESS;
+    Node* traversal;
+    traversal = element;
+    
+    while (traversal->next != NULL)
+    {
+        traversal = traversal->next;
+        printf("%d\n", traversal->data);
+    }
+    
+    return EXIT_SUCCESS;
 }
 
 int insertNode(Node** element, int elementData)
 {
-	Node* temp = malloc(sizeof(Node)); // allocate node
-	temp->data = elementData;  
-	temp->next = NULL;
-	//element->next = temp;
-	
-	Node* traversal;
-	traversal = *element;
-	
-	while (traversal->next != NULL)
-	{
-	     traversal = traversal->next;
-	}
-	traversal->next = NULL;
-	
-	return EXIT_SUCCESS;
+    Node* temp = malloc(sizeof(Node)); // allocate node
+    temp->data = elementData;  
+    temp->next = NULL;
+    // element->next = temp;
+    
+    return EXIT_SUCCESS;
 }
 
 
 int main(void)
 {
-	Node* theList;  //initialize head node
-	theList->data = 0; //dummy data
-	theList->next = NULL; //initially points nowhere
-	
-	insertNode(&theList, 1);
-	insertNode(&theList, 2);
-	printData(theList);
-	
-	return EXIT_SUCCESS;
+    Node* theList;  //initialize head node
+    theList->data = 0; //dummy data
+    theList->next = NULL; //initially points nowhere
+    
+    insertNode(&theList, 1);
+  //  insertNode(&theList, 2);
+    printData(theList);
+    
+    return EXIT_SUCCESS;
 }
